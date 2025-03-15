bf95ce13601c8c802459d0235d142afe3498495a
ismael <ismaeljorge257@gmail.com>
2025-03-15 17:57:07 -0300

    add

---
+++ aulas/logica_de_programacao/aula7.py
@@ -0,0 +1,8 @@
+nome_completo = "Ismael Jorge Brandão"
+soma_dois_mais_dois = 2 + 2
+int_um = int(1)
+print(int_um, type(int(int_um)))
+print(nome_completo, soma_dois_mais_dois)
+
+#na programção não e bom  ficar repetindo codigo
+#codigo limpo nas variaveis vc tem que ser bem descritivo oq vc colocou naquele valor
---
+++ aulas/logica_de_programacao/ex_aula8.py
@@ -0,0 +1,15 @@
+nome = 'Ismael'
+sobrenome = 'Brandão'
+idade = int(19)
+ano_nascimento = 2025 - idade
+maior_idade = idade >= 18
+altura_metros = float(1.81)
+
+
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade?: ', maior_idade)
+print('Altura em metros: ', altura_metros)
